b89dcd15412a6d031775261d7f6df9ffd2b61727
Diana <diana.dev777@gmail.com>
2023-11-12 18:43:23 +0600

    feat: change web3 to ethers

---
+++ src/pages/MainPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <p>Block</p>
+  <div>
+    <!-- {{ $store.state.blocks }} -->
+  </div>
+  <block :key="block" v-for="block in $store.state.blocks" :block="block" />
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+export default {
+  mounted() {
+    this.newBlockHeaders();
+  },
+  methods: {
+    ...mapActions({
+      newBlockHeaders: 'newBlockHeaders',
+    }),
+  },
+};
+</script>
+
+<style></style>
---
+++ src/store/index.js
@@ -0,0 +1,42 @@
+import { createStore } from 'vuex';
+const ethers = require('ethers');
+const provider = new ethers.providers.JsonRpcProvider(
+  'https://eth-goerli.g.alchemy.com/v2/FY02V0D1h9LCi7Gox4qpJHREU5tNAIaq'
+);
+// const Web3 = require('web3');
+// const web3 = new Web3('wss://eth-goerli.g.alchemy.com/v2/FY02V0D1h9LCi7Gox4qpJHREU5tNAIaq');
+
+export default createStore({
+  state: {
+    blocks: [],
+  },
+  getters: {},
+  mutations: {
+    addBlock(state, newBlock) {
+      state.blocks.unshift(newBlock);
+    },
+  },
+  actions: {
+    async newBlockHeaders({ commit }) {
+      provider.on('block', async blockNumber => {
+        let block = await provider.getBlock(blockNumber);
+        let newBlock = {
+          number: block.number,
+          hash: block.hash,
+        };
+        commit('addBlock', newBlock);
+      });
+    },
+    // eslint-disable-next-line no-unused-vars
+    async getBlock({ commit }, blockNumberOrHash) {
+      if (!ethers.utils.isBytesLike(blockNumberOrHash)) {
+        blockNumberOrHash = Number(blockNumberOrHash);
+      }
+      return await provider.getBlock(blockNumberOrHash);
+    },
+    async getTransaction(txHash) {
+      return await provider.getTransaction(txHash);
+    },
+  },
+  modules: {},
+});
